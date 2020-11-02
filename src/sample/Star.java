3f661aa77f4c445ca254572e0d0186ff27128918
joykirat18 <66181127+joykirat18@users.noreply.github.com>
2020-11-03 03:13:05 +0530

    Add star.java

--- src/sample/Star.java
+++ src/sample/Star.java
@@ -1,4 +1,21 @@
 package sample;
 
 public class Star {
+    private int[] position;
+
+    /**
+     * Returns whether the star has been collected or not
+     *
+     * @return boolean
+     */
+    public boolean isCollected() {
+        return false;
+    }
+
+    /**
+     * Increase the score of the game
+     */
+    public void increaseScore(Game game) {
+
+    }
 }
