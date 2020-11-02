bf8f4495c1345169d4ea5bf56564a973e4cdd218
Rohan Jain <rohan19095@iiitd.ac.in>
2020-11-03 00:58:38 +0530

    Comments added

--- src/sample/Main.java
+++ src/sample/Main.java
@@ -8,17 +8,17 @@ import javafx.stage.Stage;
 
 public class Main extends Application {
 
-	// * JAVAFX Boilerplate
-	// @Override
-	// public void start(Stage primaryStage) throws Exception {
-	// Parent root = FXMLLoader.load(getClass().getResource("sample.fxml"));
-	// primaryStage.setTitle("Hello World");
-	// primaryStage.setScene(new Scene(root, 300, 275));
-	// primaryStage.show();
-	// }
+    public static void main(String[] args) {
+        launch(args);
+    }
 
-	// public static void main(String[] args) {
-	// launch(args);
-	// }
-
+    // * JAVAFX Boilerplate
+    @Override
+    public void start(Stage primaryStage) throws Exception {
+        Parent root = FXMLLoader.load(getClass().getResource("sample.fxml"));
+        primaryStage.setTitle("Hello World");
+        primaryStage.setScene(new Scene(root, 300, 275));
+        primaryStage.show();
+    }
+    //
 }
--- src/sample/MainMenu.java
+++ src/sample/MainMenu.java
@@ -1,8 +1,12 @@
 package sample;
 
+import java.util.ArrayList;
+
 public abstract class MainMenu {
 
 	protected String mode;
+	ArrayList<Player> listOfPlayers;
+	Player currentPlayer;
 
 	public void setMode(String s) {
 		mode = s;
@@ -11,5 +15,19 @@ public abstract class MainMenu {
 	public abstract Game newGame();
 	// Main menu screen starts a new game
 	// initialise player
+
+//	another implementation
+//	abstract class obstacle
+//	interface motion
+//	see implementation of javafx snake game
+//	dynamic gui -> rotation
+//	motion -> scrollable and rotational
+//	ball composition in game list of game
+//	ball implements motion
+//	collectables switch and stars
+//	player to store score
+//	player has a list of games
+//	game and player serializable
+//	gameplay and game class
 
 }
