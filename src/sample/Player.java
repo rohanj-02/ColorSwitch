7352a9d8e5e395943b74f557aaf41d0e9faafad9
Rohan Jain <rohan19095@iiitd.ac.in>
2020-11-03 03:03:06 +0530

    Reformat Player.java

--- src/sample/Player.java
+++ src/sample/Player.java
@@ -3,49 +3,49 @@ package sample;
 import java.util.ArrayList;
 
 public class Player {
-    private String name;
-    private int highScore;
-    private ArrayList<Game> savedGames;
-    private int numberOfStars;
-    private Game currentGame;
+	private String name;
+	private int highScore;
+	private ArrayList<Game> savedGames;
+	private int numberOfStars;
+	private Game currentGame;
 
-        public String getName() {
-        return name;
-}publ
+	public String getName() {
+		return name;
+	}
 
+	public void setName(String name) {
+		this.name = name;
+	}
 
+	public int getHighScore() {
+		return highScore;
+	}
 
+	public void setHighScore(int highScore) {
+		this.highScore = highScore;
+	}
 
+	public ArrayList<Game> getSavedGame() {
+		return savedGames;
+	}
 
+	public void setSavedGame(ArrayList<Game> savedGame) {
+		this.savedGames = savedGame;
+	}
 
+	public int getNumberOfStars() {
+		return numberOfStars;
+	}
 
-                                             ic void setName(String name) {
-    this.name = name;
-}public int getHighScore() {
-    return highScore;
-}public void setHighScore(int highScore) {
-    this.highScore = highScore;
-}public ArrayList<Game> getSavedGame() {
-    return savedGames;
-}publ
+	public void setNumberOfStars(int numberOfStars) {
+		this.numberOfStars = numberOfStars;
+	}
 
+	public Game getCurrentGame() {
+		return currentGame;
+	}
 
-
-
-
-
-
-
-
-
-                                                       ic void setSavedGame(ArrayList<Game> savedGame) {
-    this.savedGames = savedGame;
-}public int getNumberOfStars() {
-    return numberOfStars;
-}public void setNumberOfStars(int numberOfStars) {
-    this.numberOfStars = numberOfStars;
-}public Game getCurrentGame() {
-    return currentGame;
-}public void setCurrentGame(Game currentGame) {
-    this.currentGame = currentGame;
-}}
+	public void setCurrentGame(Game currentGame) {
+		this.currentGame = currentGame;
+	}
+}
