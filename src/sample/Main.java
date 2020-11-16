2ad2050ad8dc84212f004eb2f2d9182160b8d272
joykirat18 <66181127+joykirat18@users.noreply.github.com>
2020-11-16 22:19:16 +0530

    Add Line

--- src/sample/Main.java
+++ src/sample/Main.java
@@ -6,7 +6,10 @@ import javafx.scene.Group;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
+import javafx.scene.layout.Pane;
 import javafx.stage.Stage;
+
+import java.awt.*;
 
 public class Main extends Application {
 
@@ -22,21 +25,23 @@ public class Main extends Application {
         // Add circle object
         Group root = new Group();
 //        Circle myCircle = new Circle(new Point(100,100), 30);
-        Triangle myTriangle = new Triangle(new Point(100,100), 120);
+        //Triangle myTriangle = new Triangle(new Point(100,100), 120);
+        LineObstacle line = new LineObstacle(new Point(0,100), 500);
         Button button = new Button("Show Circle");
         Button button2 = new Button("Rotate Circle");
-        button.setOnMouseClicked(e -> myTriangle.renderTriangle(root));
-        button2.setOnMouseClicked(e -> myTriangle.rotate());
+        button.setOnMouseClicked(e -> line.render(root));
+        button2.setOnMouseClicked(e -> line.play());
         button.setLayoutX(200);button.setLayoutY(200);
         button2.setLayoutX(300);button2.setLayoutY(100);
-        root.getChildren().addAll(myTriangle.triangleRoot.getChildren());
+        root.getChildren().addAll(line.getLineRoot().getChildren());
         root.getChildren().add(button2);
         root.getChildren().add(button);
+//        root.getChildren().add(line.getLineRoot());
 
         primaryStage.setTitle("Hello World");
         primaryStage.setScene(new Scene(root, 500, 500));
         primaryStage.show();
-        myTriangle.rotate();
+        //myTriangle.rotate();
     }
     //
 }
